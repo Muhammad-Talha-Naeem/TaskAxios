6f97b831b2cbb6e17665bd3a16921eb925ce6811
Muhammad-Talha-Naeem <75784944+Muhammad-Talha-Naeem@users.noreply.github.com>
2021-09-01 11:12:11 +0500

    Add files via upload

---
+++ src/components/task.vue
@@ -0,0 +1,27 @@
+<template>
+<div>
+    <table>
+        <tr v-for="product in products" v-bind:key="product.id">
+            <td><img v-bind:src="product.imgUrl" width="300"> </td>
+            <td>{{product.name}}</td>
+            <td>{{product.price}}</td>
+        </tr>
+    </table>
+</div>
+</template>
+<script>
+
+export default {
+    name: "Task-axios",    
+    data(){
+        return{products:[]}
+    },
+    mounted(){
+        fetch('http://localhost:3000/products')
+        .then(response=> response.json())
+        .then(data => this.products = data)
+        .catch(err=>err.message)
+
+    }
+}
+</script>
